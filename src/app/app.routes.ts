3bafd21cc7134afcdec8542bf4e0b0dfd1e3a0cd
ilip <1138657335@qq.com>
2024-07-02 09:22:46 +0800

    add patient component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PatientComponent } from './patient/patient.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'patient', component: PatientComponent},
+];
